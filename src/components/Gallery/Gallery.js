b5b9c1bfca50f47d392b00d4e145566fd2d0c67d
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-11-26 22:04:00 -0700

    gallery and cursor working

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 import images from './images.js';
-import isotope from 'isotope-layout';
+import full from '../../assets/cursor_full.png';
+import thirds from '../../assets/cursor_thirds.png';
 
 export default class Gallery extends Component {
+  constructor() {
+    super()
+    this.state = {
+      thirds: false
+    }
+  }
 
-  componentDidMount() {
-    $('.gallery_image').isotope({
-      layoutMode: 'masonryHorizontal',
-      masonryHorizontal: {
-        rowHeight: 50
-      }
-    })
+  changeView = () => {
+    this.setState({thirds: !this.state.thirds})
   }
 
   render() {
 
+    let gallery_view = this.state.thirds ? {
+      "width": Math.floor(images.length / 3 * 250 ) + 'px',
+      "flexWrap": "wrap",
+      "cursor": "url(" + full + "), auto"
+    } : {
+      "cursor": "url(" + thirds  + "), auto"
+    }
+
+    let gallery_image_view = this.state.thirds ? {
+      "height": "32%",
+      "margin": "5px"
+    } : {
+      "height": "80vh",
+      "marginRight": "10px"
+    }
+
     const allImages = images.map((e, i) => (
-      <div key={i} className='gallery_image'>
-        <img src={e}/>
+      <div key={i} style={ gallery_image_view }>
+        <img src={e} alt='ocean'/>
       </div>
     ))
-
+ 
     return (
       <div>
-        { allImages }
-      </div>
+        <div className="gallery" 
+          style={ gallery_view } 
+          onClick={ this.changeView }>
+          { allImages }
+        </div>
+      </div> 
     )
   }
 }
