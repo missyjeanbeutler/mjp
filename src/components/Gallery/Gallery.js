a8a6d32715d5a0f6c1f679afcb513a1a61550e32
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-12-09 00:03:04 -0700

    transition between thirds and full using transtion motion

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
+import { StaggeredMotion, TransitionMotion, spring } from 'react-motion';
 import oceans from './oceans_images.js';
 
 export default class Gallery extends Component {
+  constructor() {
+    super()
+    this.state = {
+      thirds: true,
+      fade: false,
+      num: 0
+    }
+  }
 
-  render() {
+  willLeave() {
+    return {opacity: spring(0)}
+  }
 
-    const images = oceans.map((e, i) => (
-      <div key={i} className='gallery_image'>
-        <img src={e}/>
-      </div>
-    ))
+  changeSize = (e) => {
+    this.setState(prev => {
+      return {
+        fade: !prev.fade,
+        num: prev.num + 1
+      }
+    })
+    setTimeout(() => {
+      this.setState(prev => { 
+        return { 
+          fade: !prev.fade,
+          thirds: !prev.thirds
+        }
+      })
+    })
+  }
+
+
+
+render() {
+  
+    let styles = { height: this.state.thirds ? 'calc(100% / 3 - 10px)' : '90vh' }
 
     return (
       <div>
-        { images }
+        {this.state.fade ? null :
+        <TransitionMotion 
+          styles={[{key: this.state.num + '', style: {opacity: spring(1)}}]}
+          willLeave={this.willLeave}>
+            {(motion_style) => (
+              <div style={{...motion_style[0].style}}>
+                <StaggeredMotion
+                  defaultStyles={oceans.map(e => { return {o: 0}})}
+                  styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+                    return i === 0 ? {o: spring(1)} : {o: spring(prevInterpolatedStyles[i - 1].o)}
+                  })}>
+                  {interpolatingStyles => 
+                    <div className="gallery_container">
+                      {interpolatingStyles.map((style, i) => (
+                          <div key={i} className='gallery_image_container' 
+                              style={{opacity: style.o, ...styles}}
+                              onClick={this.changeSize}>
+                            <img src={oceans[i]} alt='oceans'/>
+                          </div>
+                        )
+                      )}
+                    </div>
+                  }
+                </StaggeredMotion>
+              </div>
+            )}
+        </TransitionMotion>}
       </div>
     )
   }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { TransitionMotion, spring } from 'react-motion';
 
-export default function Home() {
-  return (
-    <div>
-      <div className='background'>
+export default class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      images: ['cabo-2.jpg', 'jamaica-1.jpg', 'jamaica-3.jpg', 'jamaica-4.jpg'],
+      num: 0,
+      fade: false
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({num: this.state.num === 3 ? 0 : this.state.num + 1, fade: true})
+      this.setState({fade: false})
+    }, 3000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    this.setState({fade: !this.state.fade})
+  }
+
+  willLeave() {
+    return {opacity: spring(0)}
+  }
+
+  render() { 
+
+    let style = {background: `url(${require(`../../assets/images/oceans/${this.state.images[this.state.num]}`)}) center no-repeat`, backgroundSize: 'cover' }
+
+    return (
+      <div>
+        {this.state.fade ? null : <TransitionMotion 
+          styles={[{key: this.state.images[this.state.num], style: {opacity: spring(1)}}]}
+          willLeave={this.willLeave}
+          defaultStyles={[{key: this.state.images[this.state.num], style: {opacity: 0}}]}>
+            {(motion_style) => (
+              <div className='home_background' style={{...motion_style[0].style, ...style}} transition={this.transition}/>
+            )}
+        </TransitionMotion>}
       </div>
-    </div>
-  )
+    )
+  }
 }
